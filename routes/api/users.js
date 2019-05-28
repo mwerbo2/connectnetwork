7ad205cb2b5da3a6505542dc5e70a9d1d59d62d4
Michael W <mwerbo2@gmail.com>
2019-05-28 14:25:56 -0500

    Set up validation error handler

--- routes/api/users.js
+++ routes/api/users.js
@@ -20,7 +20,7 @@ router.post(
   (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array()});
     }
     res.send("User route");
   }
