6a6202948fba5e91f25da059e763f4c474df6baf
Michael W <mwerbo2@gmail.com>
2019-06-04 22:05:30 -0500

    Add in unlike route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -104,8 +104,6 @@ router.delete("/:id", auth, async (req, res) => {
 // @route   Put api/posts/like/:id
 // @desc    Like a post
 // @access  Private
-module.exports = router;
-
 router.put("/like/:id", auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
@@ -123,3 +121,33 @@ router.put("/like/:id", auth, async (req, res) => {
     return res.status(500).json({ msg: "Server Error" });
   }
 });
+
+// @route   Put api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    // Check if post is already liked
+    if (
+      (post.likes.filter(
+        like => like.user.toString() === req.user.id
+      ).length = 0)
+    ) {
+      return res.status(400).json({ msg: "Post has not yet been liked" });
+    }
+
+    // Get the remove index
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+module.exports = router;
