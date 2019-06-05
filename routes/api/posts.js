fa270555ebc3799ef49dabefa4f78d96637ed7f6
Michael W <mwerbo2@gmail.com>
2019-06-04 20:19:44 -0500

    Delete post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -75,4 +75,29 @@ router.get("/:id", auth, async (req, res) => {
   }
 });
 
+// @route   Delete api/posts/:post_id
+// @desc    Delete post by Id
+// @access  Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //Check which user is deleting
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+    await post.remove();
+    res.json({ msg: "Post deleted" });
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    // res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
