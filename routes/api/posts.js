e9d8ee980b194d6d4f84a6f3b3fe04df7f7cb591
Michael W <mwerbo2@gmail.com>
2019-06-04 21:51:16 -0500

    Add like post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -75,7 +75,7 @@ router.get("/:id", auth, async (req, res) => {
   }
 });
 
-// @route   Delete api/posts/:post_id
+// @route   Delete api/posts/:id
 // @desc    Delete post by Id
 // @access  Private
 router.delete("/:id", auth, async (req, res) => {
@@ -100,4 +100,26 @@ router.delete("/:id", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route   Put api/posts/like/:id
+// @desc    Like a post
+// @access  Private
 module.exports = router;
+
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    // Check if post is already liked
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ msg: "Server Error" });
+  }
+});
