797113cfe5b4d62419faa8179aa3ec58bef96e35
Michael W <mwerbo2@gmail.com>
2019-06-05 16:58:08 -0500

    CRA clean up and file setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Navbar from "./components/layouts/Navbar";
+import Landing from "./components/layouts/Landing";
+import "./App.css";
+
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <Landing />
+  </Fragment>
+);
+export default App;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 //connect database
 connectDB();
