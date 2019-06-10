eb6790c14f356dccd1114d2efc67868fa75930e4
Michael W <mwerbo2@gmail.com>
2019-06-09 22:02:05 -0500

    Reducer for storing token and validating

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,13 +1,29 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from "./types";
-
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
+} from "./types";
+import setAuthToken from "../utils/setAuthToken";
 // LOAD USER
 export const loadUser = () => async dispatch => {
-
-}
-
-
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get('/api/auth');
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    })
+  }
+};
 
 //Register
 export const register = ({ name, email, password }) => async dispatch => {
@@ -24,7 +40,7 @@ export const register = ({ name, email, password }) => async dispatch => {
     dispatch({
       type: REGISTER_SUCCESS,
       payload: res.data
-    });
+  });
   } catch (err) {
     const errors = err.response.data.errors;
     if (errors) {
