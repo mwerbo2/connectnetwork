6ad1641a27f14bddbc27852b754a70e4a23e63d9
Michael W <mwerbo2@gmail.com>
2019-06-11 19:00:13 -0500

    Create private route component router

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Dashboard from "./components/dashboard/Dashboard";
 import Alert from "./components/layouts/Alert";
+import PrivateRoute from "./components/routing/PrivateRoute";
 import "./App.css";
 // Redux
 import { Provider } from "react-redux";
@@ -33,7 +34,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
