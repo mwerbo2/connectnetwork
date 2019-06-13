bd73ed3c391e996d0608a0c9d88db9c8e7a123b2
Michael W <mwerbo2@gmail.com>
2019-06-13 12:57:47 -0500

    Add in dashboard action, and dispatch into dashboard component

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,16 +1,30 @@
+import { GET_PROFILE, PROFILE_ERROR } from "../actions/types";
+
 const initalState = {
-    profile: null,
-    profiles: [],
-    repos: [],
-    loading: true,
-    error: {}
-}
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {}
+};
 
 export default function(state = initalState, action) {
-    const { type, payload } = action;
+  const { type, payload } = action;
 
-    switch(type) {
-        
-
-    }
+  switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false
+      };
+    default:
+      return state;
+  }
 }
