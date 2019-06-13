d07e743c41e3d8d01f8f13b0814b7fd5c826bec5
Michael W <mwerbo2@gmail.com>
2019-06-13 13:46:33 -0500

    Set up ternary for user w/wo profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
+import Spinner from "../layouts/Spinner";
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading }
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
-  return <div>Dashboard</div>;
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <hi className="large text-primary">Dashboard</hi>
+      <p className="lead">
+        <i className="fas fas-user"> Welcome {user && user.name}</i>
+      </p>
+      {profile !== null ? (
+        <Fragment>Has</Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not set up a profile, please set one up.</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Dashboard.propTypes = {
