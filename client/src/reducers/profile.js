dee8f1ed85537e46fc8fcc2bc3ab4ee49e236c51
Michael W <mwerbo2@gmail.com>
2019-06-13 12:02:13 -0500

    Add in profile types and reducer

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,16 @@
+const initalState = {
+    profile: null,
+    profiles: [],
+    repos: [],
+    loading: true,
+    error: {}
+}
+
+export default function(state = initalState, action) {
+    const { type, payload } = action;
+
+    switch(type) {
+        
+
+    }
+}
