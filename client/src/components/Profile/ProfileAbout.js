4a9df1a348e13a187f1ebdfdff44bdd4797491f9
Michael W <mwerbo2@gmail.com>
2019-06-30 23:34:10 -0500

    Addin expereince

--- client/src/components/Profile/ProfileAbout.js
+++ client/src/components/Profile/ProfileAbout.js
@@ -7,29 +7,29 @@ const ProfileAbout = ({
     skills,
     user: { name }
   }
-}) => {
-  return (
-    <div class="profile-about bg-light p-2">
-      {bio && (
-        <Fragment>
-          <h2 class="text-primary">{name.trim().split(" ")[0]}s Bio</h2>
-          <p>{bio}</p>
-          <div class="line" />
-        </Fragment>
-      )}
+}) => (
+  <div class="profile-about bg-light p-2">
+    {bio && (
+      <Fragment>
+        <h2 class="text-primary">{name.trim().split(" ")[0]}s Bio</h2>
+        <p>{bio}</p>
+        <div class="line" />
+      </Fragment>
+    )}
 
-      <h2 class="text-primary">Skill Set</h2>
-      <div class="skills">
-        {skills.map((skill, index) => (
-          <div key={index} className="p-1">
-            <i className="fas fa-check" /> {skill}
-          </div>
-        ))}
-      </div>
+    <h2 class="text-primary">Skill Set</h2>
+    <div class="skills">
+      {skills.map((skill, index) => (
+        <div key={index} className="p-1">
+          <i className="fas fa-check" /> {skill}
+        </div>
+      ))}
     </div>
-  );
-};
+  </div>
+);
 
-ProfileAbout.propTypes = {};
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
 
 export default ProfileAbout;
