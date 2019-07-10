11b2937d8f7dfcaa92ce90d1b79fcc5575a8b488
Michael W <mwerbo2@gmail.com>
2019-07-10 18:58:07 -0500

    Added in the github stars, forks and badges

--- client/src/components/Profile/ProfileGithub.js
+++ client/src/components/Profile/ProfileGithub.js
@@ -5,30 +5,47 @@ import Spinner from "../layouts/Spinner";
 import { getGithubRepos } from "../../actions/profile";
 
 const ProfileGithub = ({ username, getGithubRepos, repos }) => {
+  useEffect(() => {
+    getGithubRepos(username);
+  }, [getGithubRepos]);
 
-    useEffect(() => {
-        getGithubRepos(username)
-    }, [getGithubRepos]);
-
-
-return (
+  return (
     <div className="profile-github">
-        <h2 className="text-primary my-1">Github Repos</h2>
-        {repos === null ? <Spinner /> : (
-            repos.map(repo => (
-                <div key={repo._id} className='repo bg-white p-1 my-1'>
-                    <div>
-                        <h4>
-                            <a href={repo.html_url} target='_blank' rel='noopener noreferrer'>{repo.name}</a>
-                        </h4>
-                    </div>
-                </div>
-            ))
-        )}
-</div>
-)
-
-
+      <h2 className="text-primary my-1">Github Repos</h2>
+      {repos === null ? (
+        <Spinner />
+      ) : (
+        repos.map(repo => (
+          <div key={repo._id} className="repo bg-white p-1 my-1">
+            <div>
+              <h4>
+                <a
+                  href={repo.html_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {repo.name}
+                </a>
+              </h4>
+              <p>{repo.description}</p>
+            </div>
+            <div>
+              <ul>
+                <li clasName="badge badge-primary">
+                  Stars: {repo.stargazers_count}
+                </li>
+                <li clasName="badge badge-dark">
+                  Watchers: {repo.watcher_count}
+                </li>
+                <li clasName="badge badge-light">Forks: {repo.forks_count}</li>
+              </ul>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
 
 ProfileGithub.propTypes = {
   getGithubRepos: PropTypes.func.isRequired,
