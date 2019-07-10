ad2bce46e39e6e9b5879d1b89c987af23124e27f
Michael W <mwerbo2@gmail.com>
2019-07-10 18:53:49 -0500

    Github repo built

--- client/src/components/Profile/ProfileGithub.js
+++ client/src/components/Profile/ProfileGithub.js
@@ -6,22 +6,29 @@ import { getGithubRepos } from "../../actions/profile";
 
 const ProfileGithub = ({ username, getGithubRepos, repos }) => {
 
-    useEffect(( => {
+    useEffect(() => {
         getGithubRepos(username)
-    }, [getGithubRepos(username)]))
-  <div className="profile-github">
-      <h2 className="text-primary my-1">Github Repos</h2>
-      {repos === null ? <Spinner /> : (
-          repos.map(repo => (
-              <div key={repo._id} className='repo bg-white p-1 my-1'>
-                  <h4>
-                      <a href={repo.html_url} target='_blank' rel='noopener noreferrer'></a>
-                  </h4>
-              </div>
-          ))
-      )
-  </div>
-};
+    }, [getGithubRepos]);
+
+
+return (
+    <div className="profile-github">
+        <h2 className="text-primary my-1">Github Repos</h2>
+        {repos === null ? <Spinner /> : (
+            repos.map(repo => (
+                <div key={repo._id} className='repo bg-white p-1 my-1'>
+                    <div>
+                        <h4>
+                            <a href={repo.html_url} target='_blank' rel='noopener noreferrer'>{repo.name}</a>
+                        </h4>
+                    </div>
+                </div>
+            ))
+        )}
+</div>
+)
+
+
 
 ProfileGithub.propTypes = {
   getGithubRepos: PropTypes.func.isRequired,
